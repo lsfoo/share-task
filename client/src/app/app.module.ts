85c1bd3ca672cc7243b7814e416ee5719d79f183
lsfoo <lsfoo@qq.com>
2019-01-07 17:29:44 +0800

    k

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,39 +1,61 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {A11yModule} from '@angular/cdk/a11y';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling'
+import { A11yModule } from '@angular/cdk/a11y'
+import { CdkTableModule } from '@angular/cdk/table'
+import { CdkTreeModule } from '@angular/cdk/tree'
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule,
-  MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule,
-  MatDialogModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatGridListModule,
-  MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatPaginatorModule,
-  MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule,
-  MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule,
-  MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule} from '@angular/material';
-import 'hammerjs';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
-import { WechatTaskComponent } from './task/wechat-task/wechat-task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule
+} from '@angular/material'
+import 'hammerjs'
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    WechatTaskComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-     A11yModule,
+    A11yModule,
     CdkTableModule,
     CdkTreeModule,
     DragDropModule,
@@ -72,7 +94,8 @@ import { WechatTaskComponent } from './task/wechat-task/wechat-task.component';
     MatToolbarModule,
     MatTooltipModule,
     MatTreeModule,
-    ScrollingModule, A11yModule,
+    ScrollingModule,
+    A11yModule,
     CdkTableModule,
     CdkTreeModule,
     DragDropModule,
@@ -116,4 +139,4 @@ import { WechatTaskComponent } from './task/wechat-task/wechat-task.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
