2d7a9a0f673a68350730be763b912f067de8e241
lsfoo <lsfoo@qq.com>
2019-01-08 23:36:09 +0800

    发布任务

--- client/src/app/tasks/tasks-routing.module.ts
+++ client/src/app/tasks/tasks-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { TaskListComponent } from './task-list/task-list.component'
 import { TaskDetailsComponent } from './task-details/task-details.component'
+import { TaskAddComponent } from './task-add/task-add.component'
 
 const routes: Routes = [
-  { path: '', component: TaskListComponent },
-  { path: 'task-details/:id', component: TaskDetailsComponent }
-]
+  { path: "", component: TaskListComponent },
+  { path: "task-details/:id", component: TaskDetailsComponent },
+  { path: "task-add", component: TaskAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TasksRoutingModule {}
+export class TasksRoutingModule { }
--- client/src/app/tasks/tasks.module.ts
+++ client/src/app/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 
 import { TasksRoutingModule } from './tasks-routing.module'
 import { TaskListComponent } from './task-list/task-list.component'
-import { TaskDetailsComponent } from './task-details/task-details.component'
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskAddComponent } from './task-add/task-add.component'
 
 @NgModule({
-  declarations: [TaskListComponent, TaskDetailsComponent],
+  declarations: [TaskListComponent, TaskDetailsComponent, TaskAddComponent],
   imports: [CommonModule, TasksRoutingModule]
 })
 export class TasksModule {}
