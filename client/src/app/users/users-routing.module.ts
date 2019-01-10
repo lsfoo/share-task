75d4944fd95de518d06409260a15a5041f58c579
lsfoo <lsfoo@qq.com>
2019-01-10 17:43:33 +0800

    job

--- client/src/app/users/users-routing.module.ts
+++ client/src/app/users/users-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './login/login.component'
 import { RegisterComponent } from './register/register.component'
 import { JobListComponent } from './job-list/job-list.component'
+import { JobDetailsComponent } from './job-details/job-details.component'
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'job-list', component: JobListComponent }
+  { path: 'job-list', component: JobListComponent },
+  { path: 'job-details/:id', component: JobDetailsComponent }
 ]
 
 @NgModule({
--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common'
 import { UsersRoutingModule } from './users-routing.module'
 import { RegisterComponent } from './register/register.component'
 import { LoginComponent } from './login/login.component';
-import { JobListComponent } from './job-list/job-list.component'
+import { JobListComponent } from './job-list/job-list.component';
+import { JobDetailsComponent } from './job-details/job-details.component'
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent, JobListComponent],
+  declarations: [RegisterComponent, LoginComponent, JobListComponent, JobDetailsComponent],
   imports: [CommonModule, UsersRoutingModule]
 })
 export class UsersModule {}
