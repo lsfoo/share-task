9aed14d22831bbbf9a51403efb83030f728492a9
lsfoo <lsfoo@qq.com>
2019-01-17 17:51:15 +0800

    fuck

--- models/store/src/main/java/com/jhipster/demo/store/domain/enumeration/OrderItemStatus.java
+++
@@ -1,8 +0,0 @@
-package com.jhipster.demo.store.domain.enumeration;
-
-/**
- * The OrderItemStatus enumeration.
- */
-public enum OrderItemStatus {
-    AVAILABLE, OUT_OF_STOCK, BACK_ORDER
-}
--- models/store/src/main/java/com/jhipster/demo/store/domain/enumeration/Size.java
+++
@@ -1,8 +0,0 @@
-package com.jhipster.demo.store.domain.enumeration;
-
-/**
- * The Size enumeration.
- */
-public enum Size {
-    S, M, L, XL, XXL
-}
